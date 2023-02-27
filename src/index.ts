ca8a6f3f3ab5e671f26b04de71b5f7076931f5f8
ismi-abbas <muhdabbas98@gmail.com>
2023-02-27 21:59:43 +0800

    Restructrure

--- src/index.ts
+++ src/index.ts
@@ -5,52 +5,47 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const app = new Elysia()
-
-app.use(swagger())
-
-app.get('/', () => {
-    return {
-        status: '200',
-        message: 'Welcome to Elysia'
-    }
-})
-
-app.post('/signup', ({ body, set }: {
-    body: any, set: Context['set']
-}) => {
-    const { username, email } = body
-    prisma.users.create({
-        data: {
-            username,
-            email,
-        }
-    }).then((user) => {
-        console.log({ user })
-        set.status = 200
+    .use(swagger())
+    .get('/', () => {
         return {
             status: '200',
-            message: 'User created successfully',
-            user: user
-        }
-    }).catch((err) => {
-        console.log({ err })
-        set.status = 401
-        return {
-            status: '401',
-            message: 'User creation failed'
+            message: 'Welcome to Elysia'
         }
     })
-}, {
-    schema: {
-        body: t.Object({
-            username: t.String(),
-            email: t.String()
+    .post('/signup', ({ body, set }: {
+        body: any, set: Context['set']
+    }) => {
+        const { username, email } = body
+        prisma.users.create({
+            data: {
+                username,
+                email,
+            }
+        }).then((user) => {
+            console.log({ user })
+            set.status = 200
+            return {
+                status: '200',
+                message: 'User created successfully',
+                user: user
+            }
+        }).catch((err) => {
+            console.log({ err })
+            set.status = 401
+            return {
+                status: '401',
+                message: 'User creation failed'
+            }
         })
-    }
-})
+    }, {
+        schema: {
+            body: t.Object({
+                username: t.String(),
+                email: t.String()
+            })
+        }
+    })
+    .listen(3000)
 
-
-app.listen(3000, () => {
-    console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
-})
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
 
