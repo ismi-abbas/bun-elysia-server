ed907bb6e4a59a8ab4d33a68aad44f06edf50be3
ismi-abbas <muhdabbas98@gmail.com>
2023-02-27 12:16:23 +0800

    trying some zod

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Elysia, type Context } from 'elysia'
 import { swagger } from '@elysiajs/swagger'
+import { schema } from '../utils/loginSchema'
+
 
 const app = new Elysia()
 
@@ -9,18 +11,27 @@ app.get('/', () => 'Hello Elysia')
 
 app.get('/abbas', () => { name: 'Abbas' })
 
-app.post('login', ({ body, set }: { body: any, set: Context['set'] }) => {
-    if (body.username !== 'abbas' && body.password !== '123456') {
-        set.status = 401
-        return {
-            status: '200',
-            message: 'Login successful'
+app.post('/login', ({ body, set }: { body: any, set: Context['set'] }) => {
+    try {
+        const { username, password } = schema.parse(body)
+        if (username !== 'abbas' && password !== '123456') {
+            set.status = 401
+            return {
+                status: '200',
+                message: 'Login successful'
+            }
+        } else {
+            set.status = 401
+            return {
+                status: '401',
+                message: 'Login failed'
+            }
         }
-    } else {
-        set.status = 401
+    } catch (error: any) {
+        set.status = 400
         return {
-            status: '401',
-            message: 'Login failed'
+            status: '400',
+            message: JSON.parse(error)
         }
     }
 
